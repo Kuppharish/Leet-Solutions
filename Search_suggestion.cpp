9e46377a7e198ea1a075107d11ced338efe7e14b
Harish Kuppam <kuppamharish28@gmail.com>
2020-08-07 21:08:53 -0500

    Create Search_suggestion.cpp

---
+++ Search_suggestion.cpp
@@ -0,0 +1,18 @@
+    vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
+        vector<vector<string>> res;
+        sort(products.begin(),products.end());
+        string cur = "";
+        auto it = products.begin();
+        for(char c:searchWord){
+            cur+=c;
+            vector<string> suggested;
+            it = lower_bound(it,products.end(),cur);
+            for(int j=0;j<3&&it+j!=products.end();++j){
+                string& s = *(it+j);
+                if(s.find(cur)) break; //checking if first subset of cur is in 0 index
+                suggested.push_back(s);
+            }
+            res.push_back(suggested);
+        }
+        return res;
+    }
